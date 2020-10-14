e88034111112fec857ac312be3cdc14028eb3005
denizyagiz <45318039+denizyagiz@users.noreply.github.com>
2020-10-14 15:46:56 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+// Class: CS 5000
+// Term: Spring 2020
+// Name: Deniz Yagiz
+// Instructor: Dr. Haddad
+// Assignment: 1
+// IDE Name: Jgrasp
+import java.util.Scanner; //import scanner class
+public class HelloWorld
+{
+   public static void main(String args[]) //program main method
+   {
+      Scanner s = new Scanner(System.in);
+      System.out.print("Please enter your name: "); //prompt user for input
+      String name = s.nextLine(); //read user input
+      System.out.println("Hello " + name + "!"); //print out your message
+   }
+}
